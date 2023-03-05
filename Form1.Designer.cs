175333ed623a9a5db556b2e9dc3c49936c6906b9
perpet <perpet99@gmail.com>
2023-03-06 06:45:52 +0900

    init code

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace TSimulater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.pathTextbox = new System.Windows.Forms.TextBox();
+            this.eventBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(554, 184);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // pathTextbox
+            // 
+            this.pathTextbox.Location = new System.Drawing.Point(229, 184);
+            this.pathTextbox.Name = "pathTextbox";
+            this.pathTextbox.Size = new System.Drawing.Size(251, 21);
+            this.pathTextbox.TabIndex = 1;
+            // 
+            // eventBtn
+            // 
+            this.eventBtn.Location = new System.Drawing.Point(554, 295);
+            this.eventBtn.Name = "eventBtn";
+            this.eventBtn.Size = new System.Drawing.Size(75, 23);
+            this.eventBtn.TabIndex = 2;
+            this.eventBtn.Text = "event";
+            this.eventBtn.UseVisualStyleBackColor = true;
+            this.eventBtn.Click += new System.EventHandler(this.eventBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(554, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "sentry";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.sentryBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(95, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "60";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "drive";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "update tick(sec)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "tick change";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 360);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1014, 208);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 568);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.eventBtn);
+            this.Controls.Add(this.pathTextbox);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox pathTextbox;
+        private System.Windows.Forms.Button eventBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Globalization;
+
+namespace TSimulater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 60 * 1000;
+            pathTextbox.Text = ConfigurationManager.AppSettings["path"];
+        }
+
+        Dictionary<string, string> fileList = new Dictionary<string, string>();
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            ConfigurationManager.AppSettings["path"] = pathTextbox.Text;
+
+
+            AddLog(startBtn.Text);
+
+
+            if ( startBtn.Text == "start")
+            {
+                startBtn.Text = "stop";
+
+
+                timer1.Start();
+
+                timer1_Tick(null,null);
+            }
+            else
+            {
+                startBtn.Text = "start";
+                timer1.Stop();
+            }
+
+            
+
+        }
+
+
+
+        private void eventBtn_Click(object sender, EventArgs e)
+        {
+            var destRecent = Path.Combine(pathTextbox.Text, @"TeslaCam\RecentClips\");
+            var destSave = Path.Combine(pathTextbox.Text, @"TeslaCam\SavedClips\");
+
+            moveFile(destRecent, destSave);
+        }
+
+
+        public async Task CopyFilesAsync(string SourceName, string DestinationName)
+        {
+            
+            {
+                using (FileStream sourceStream = File.Open(SourceName, FileMode.Open))
+                {
+                    using (FileStream destinationStream = File.Create(DestinationName))
+                    {
+                        await sourceStream.CopyToAsync(destinationStream);
+                    }
+                }
+            }
+        }
+
+
+
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+
+            AddLog("start write recentclips");
+
+            
+
+            var destRecent = Path.Combine(pathTextbox.Text , @"TeslaCam\RecentClips\");
+            //var destSave = Path.Combine(pathTextbox.Text , @"TeslaCam\SavedClips\");
+
+            var now = DateTime.Now;
+
+            Directory.CreateDirectory(destRecent);
+
+            foreach (var item in fileList)
+            {
+                
+
+                var dest = Path.Combine(destRecent, now.ToString("yyyy-MM-dd_HH-mm-ss-") + item.Key+".mp4");
+
+                AddLog($"write file {dest}");
+
+
+
+                await CopyFilesAsync(item.Value,dest);
+            }
+
+            AddLog("end write recentclips");
+
+            AddLog($"wait time({timer1.Interval/1000} sec)");
+        }
+
+        private void AddLog(string v)
+        {
+            listBox1.Items.Insert(0, v);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fileList.Add("front", "sample.mp4");
+            fileList.Add("back", "sample.mp4");
+            fileList.Add("left_repeater", "sample.mp4");
+            fileList.Add("right_repeater", "sample.mp4");
+        }
+
+        private void sentryBtn_Click(object sender, EventArgs e)
+        {
+            var destRecent = Path.Combine(pathTextbox.Text, @"TeslaCam\RecentClips\");
+            var destSave = Path.Combine(pathTextbox.Text, @"TeslaCam\SentryClips\");
+
+            moveFile(destRecent,destSave);
+        }
+
+
+        private void moveFile(string destRecent, string destSave)
+        {
+
+            var list = Directory.GetFiles(destRecent, "*.mp4");
+
+            var flist = list.Select(item => new FileInfo(item)).OrderBy(item => item.CreationTime).ToList();
+
+            destSave = Path.Combine(destSave, DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+
+            Directory.CreateDirectory(destSave);
+
+            for (int i = 0; i < 40 && i < flist.Count; i++)
+            {
+                var dest = Path.Combine(destSave, flist[i].Name);
+
+                AddLog($"move file {dest}");
+
+
+                File.Move(flist[i].FullName, dest);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int value))
+            {
+                timer1.Interval = value * 1000;
+
+                MessageBox.Show("update success");
+
+            }
+            else
+            {
+                MessageBox.Show("update fail");
+            }
+
+
+        }
+    }
+}
